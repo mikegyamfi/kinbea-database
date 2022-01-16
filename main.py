d6b25148f413d1902b7af9c53943b15ea09e648d
Michael <michaelgyamfi696@gmail.com>
2022-01-16 07:42:36 +0000

    Changed Integer field to Float Field

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
     total_quantity = db.Column(db.Integer, nullable=False)
-    purchase_price = db.Column(db.Real, nullable=False)
+    purchase_price = db.Column(db.Numeric(2, 2), nullable=False)
     selling_price = db.Column(db.Float(precision=2, decimal_return_scale=2), nullable=False)
     date = db.Column(db.String(100), nullable=False)
     quantity_sold = db.Column(db.Integer, nullable=False)
